8dd8cc340f8b622ac1a757568e9d0c9a359bf4a7
Calipso <calipse.antonyan@gmail.com>
2024-10-04 17:09:54 +0400

    added homework

---
+++ 02_List_comprehension/task_123.py
@@ -0,0 +1,18 @@
+# dict_1={}
+# dict_1['name']='Calipso'
+# dict_1['surname']='Antonyan'
+# dict_1['profession']='engineer'
+# print(dict_1)
+#
+# dict_2={'name':'Anna',
+#         'ID': 1000, }
+# print(dict_2)
+#
+# dict_1['name'] = 'Vagho'
+# print(dict_1)
+# dict_3 ={ 'age': 12,
+#           'is_boy':True,
+#           'countr': 'Yerevan',
+#  }
+# print(dict_3)
+
