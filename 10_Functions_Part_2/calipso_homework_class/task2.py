01835687429b0d22ca7d34cb8b640996a9361a9d
Calipso <calipse.antonyan@gmail.com>
2024-10-21 18:37:59 +0400

    added homeworck

---
+++ 10_Functions_Part_2/calipso_homework_class/task2.py
@@ -0,0 +1,24 @@
+class Parent:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.children = []
+
+    def provide_info(self):
+
+
+
+class Child:
+    def __init__(self, name, age,calm_state, hunger_state):
+        self.name = name
+        self.age = age
+        self.calm_state = calm_state
+        self.hunger_state = hunger_state
+
+        calm_state= False
+        hunger_state=True
+
+
+
+
+
