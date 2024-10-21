6d0414613063eea9aaef36cdecc2ddf2c1ade0bf
Calipso <calipse.antonyan@gmail.com>
2024-10-21 18:23:43 +0400

    added homeworck

---
+++ 10_Functions_Part_2/calipso_homework_class/task1.py
@@ -0,0 +1,14 @@
+class Student:
+    def __init__(self, first_name, last_name, group_number, learning_rate):
+        self.first_name=first_name
+        self.last_name=last_name
+        self.group_number=group_number
+        self.learning_rate=learning_rate
+
+def input_student_info():
+    first_name=input('please fill your first name: ')
+    last_name=input('please fill you last name: ')
+    group_number=input('please fill you last name: ')
+
+   learning_rate=[]
+
