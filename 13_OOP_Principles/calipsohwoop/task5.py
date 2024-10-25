02eeca57bac6bcf03957f5ce4a202927f98efec5
Calipso <calipse.antonyan@gmail.com>
2024-10-25 19:14:46 +0400

    added homeworck

---
+++ 13_OOP_Principles/calipsohwoop/task5.py
@@ -0,0 +1,28 @@
+
+import random
+
+class Cell:
+    def __init__(self,number,sign):
+        self.number=number
+        self.sign=sign
+        self.busy=False
+
+    def change_state(self,sign):
+        if not self.busy:
+            self.sign=sign
+            self.busy=True
+
+
+class Player:
+    def __init__(self,name,sign):
+        self.name=name
+        self.sign=sign
+        self.win=0
+
+    def make_move(self,board):
+        while True:
+
+
+
+
+
